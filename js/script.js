b2526a31d70b8f388af44f174fd61d3f1b8f586d
rtrikha <raunakstrikha@gmail.com>
2023-11-03 21:15:53 +0530

    v1 of opal type

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function readMore() {
+	console.log('hello');
+	const readMoreText = document.getElementById('description');
+	const readMoreButton = document.getElementById('readMoreButton');
+	readMoreText.style.maxHeight = '200px';
+	readMoreButton.style.opacity = '0';
+}
